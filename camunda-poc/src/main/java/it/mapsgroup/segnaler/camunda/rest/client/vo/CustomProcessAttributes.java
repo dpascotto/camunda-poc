f6e2c5de041b1be82e13a7774592970d747c41c4
diego.p <dpascotto@gmail.com>
2021-06-03 15:02:42 +0200

    eliminazaione parti inutili #2

--- camunda-poc/src/main/java/it/mapsgroup/segnaler/camunda/rest/client/vo/CustomProcessAttributes.java
+++
@@ -1,9 +0,0 @@
-package it.mapsgroup.segnaler.camunda.rest.client.vo;
-
-// TODO verificare se la sua eliminazione dà problemi
-@Deprecated
-public class CustomProcessAttributes {//implements CustomVariables {
-	
-	public CustomVariableValueAndType businessInputAttributes;
-
-}
