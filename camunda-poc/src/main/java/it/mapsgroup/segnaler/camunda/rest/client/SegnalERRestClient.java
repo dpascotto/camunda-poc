b9cdc8dfa16d68dcc3ff4ab784c8d413a7166038
diego.p <dpascotto@gmail.com>
2021-06-04 15:36:34 +0200

    fatta meglio l'assegnazione di un task (assignTask) senza fare il new

--- camunda-poc/src/main/java/it/mapsgroup/segnaler/camunda/rest/client/SegnalERRestClient.java
+++ camunda-poc/src/main/java/it/mapsgroup/segnaler/camunda/rest/client/SegnalERRestClient.java
@@ -351,10 +351,7 @@ public class SegnalERRestClient {
 		String taskKey = readFromInputLine("Inserisci l'id del task: ");
 		String userId = readFromInputLine("Inserisci l'id dell'utente: ");
 		
-		// Correggere questa schifezza
-		
-		Task task = new Task();
-		task.id = taskKey;
+		Task task = getTaskDetail(taskKey);
 		
 		assignTask(task, userId);
 	}
